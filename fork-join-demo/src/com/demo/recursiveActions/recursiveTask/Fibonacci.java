28267fdfb4c5a664528efe04adf87c45c25e2eb6
rishabhkashyap <kashyap.rishab@hotmail.com>
2022-03-11 19:11:40 +0530

    Calculate nth fibonacci number using RecursiveTask and fork join pool

---
+++ fork-join-demo/src/com/demo/recursiveActions/recursiveTask/Fibonacci.java
@@ -0,0 +1,33 @@
+package com.demo.recursiveActions.recursiveTask;
+//Calculate nth fibonacci number using RecursiveTask and fork join pool
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        int n = 6;
+        FibonacciTask fibonacciTask = new FibonacciTask(n);
+        System.out.println(n + "th fibonacci number = " + forkJoinPool.invoke(fibonacciTask));
+
+    }
+}
+
+class FibonacciTask extends RecursiveTask<Integer> {
+    private int n;
+
+    public FibonacciTask(int n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Integer compute() {
+        if (n <= 1) {
+            return n;
+        }
+        FibonacciTask task1 = new FibonacciTask(n - 1);
+        FibonacciTask task2 = new FibonacciTask(n - 2);
+        task1.fork();
+        return task1.join() + task2.compute();
+    }
+}
